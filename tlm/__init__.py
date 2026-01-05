39dda4872e7aae8d8c4f9f3041a27879fb14aa1f
huiwengoh <45724323+huiwengoh@users.noreply.github.com>
2026-01-05 17:04:36 -0500

    api draft

---
+++ tlm/__init__.py
@@ -0,0 +1,3 @@
+from .api import TLM as TLM
+
+__all__ = ["TLM"]
--- tlm/api.py
+++ tlm/api.py
@@ -1,31 +1,104 @@
-from typing import Any
+from typing import Any, TYPE_CHECKING
+
+import asyncio
+import sys
 
 from tlm.config.base import Config, ConfigInput
 from tlm.config.presets import WorkflowType
 from tlm.inference import InferenceResult, tlm_inference
-from tlm.types import SemanticEval, CompletionParams
+from tlm.types import SemanticEval
+
+if TYPE_CHECKING:
+    from openai.types.chat import ChatCompletion
 
 
-async def inference(
-    *,
-    openai_args: CompletionParams,
-    response: dict[str, Any] | None = None,  # TODO: support passing multiple reference completions?
-    context: str | None = None,
-    evals: list[SemanticEval] | None = None,
-    config_input: ConfigInput = ConfigInput(),
-) -> InferenceResult:
-    workflow_type = WorkflowType.from_inference_params(
-        openai_args=openai_args,
-        score=response is not None,
-        rag=(context is not None),
-        constrain_outputs=config_input.constrain_outputs,
-    )
-    model = openai_args.get("model")
-    config = Config.from_input(config_input, workflow_type, model)
-    return await tlm_inference(
-        completion_params=openai_args,
-        response=response,
-        evals=evals,
-        context=context,
-        config=config,
-    )
+def is_notebook() -> bool:
+    """Returns True if running in a notebook, False otherwise."""
+    try:
+        get_ipython = sys.modules["IPython"].get_ipython
+        return bool("IPKernelApp" in get_ipython().config)
+    except Exception:
+        return False
+
+
+class TLM:
+    def __init__(
+        self,
+        config_input: ConfigInput = ConfigInput(),
+        evals: list[SemanticEval] = None,
+    ):
+        self.config_input = config_input
+        self.evals = evals
+
+        is_notebook_flag = is_notebook()
+
+        if is_notebook_flag:
+            import nest_asyncio  # type: ignore
+
+            nest_asyncio.apply()
+
+        try:
+            self._event_loop = asyncio.get_event_loop()
+        except RuntimeError:
+            self._event_loop = asyncio.new_event_loop()
+
+    def create(
+        self,
+        *,
+        context: str | None = None,
+        evals: list[SemanticEval] | None = None,
+        **openai_kwargs: Any,
+    ) -> InferenceResult:
+        return self._event_loop.run_until_complete(
+            self._async_inference(
+                context=context,
+                evals=evals,
+                **openai_kwargs,
+            )
+        )
+
+    def score(
+        self,
+        *,
+        response: "ChatCompletion",
+        context: str | None = None,
+        evals: list[SemanticEval] | None = None,
+        **openai_kwargs: Any,
+    ) -> InferenceResult:
+        from openai.types.chat import ChatCompletion
+
+        if isinstance(response, ChatCompletion):
+            response = response.model_dump()
+
+        return self._event_loop.run_until_complete(
+            self._async_inference(
+                response=response,
+                context=context,
+                evals=evals,
+                **openai_kwargs,
+            )
+        )
+
+    async def _async_inference(
+        self,
+        *,
+        response: dict[str, Any] | None = None,
+        context: str | None = None,
+        evals: list[SemanticEval] | None = None,
+        **openai_kwargs: Any,
+    ) -> InferenceResult:
+        workflow_type = WorkflowType.from_inference_params(
+            openai_args=openai_kwargs,
+            score=response is not None,
+            rag=(context is not None),
+            constrain_outputs=self.config_input.constrain_outputs,
+        )
+        model = openai_kwargs.get("model")
+        config = Config.from_input(self.config_input, workflow_type, model)
+        return await tlm_inference(
+            completion_params=openai_kwargs,
+            response=response,
+            evals=evals,
+            context=context,
+            config=config,
+        )
